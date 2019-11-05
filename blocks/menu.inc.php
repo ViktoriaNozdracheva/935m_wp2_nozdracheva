c1364d009c8cfc219c619f5e8e5c04502a1f0e3c
ViktoriaNozdracheva <37844924+ViktoriaNozdracheva@users.noreply.github.com>
2019-11-05 12:30:19 +0300

    Laboratornaya rabota №2

---
+++ blocks/menu.inc.php
@@ -0,0 +1,12 @@
+﻿<?php
+	$menu = array(
+		"Главная"=>"index.php", 
+		"Работа №1"=>"lab_rab1.html",
+		"Работа №2"=>"lab_rab2.php");
+?>	
+
+<nav id="menu">
+	<?php
+		getMenu($menu);
+	?>
+</nav>
---
+++ blocks/top.inc.php
@@ -0,0 +1,8 @@
+﻿<div>
+	<a href="index.html"><img class="index-main-img" src="images\logo.png" alt = "'логотип университета" ></a>	
+</div>
+<div class="index-text-2"> 
+        <div><b></b> г.Рязань, Гагарина, д.59/1</div>
+		<div> <b>Телефон:</b> 8-900-955-50-55 </div>
+		<div> <b>Email:</b> University@mail.ru </div>
+</div>
